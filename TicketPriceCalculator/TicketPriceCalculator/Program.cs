f9803bbe55d52edd5df78e6d5fb171f42d6176d4
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-03 23:04:17 +0000

    AddTicket Price Calcualator App

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("------------------------------");
+Console.WriteLine("----Ticket Price Calculator---");
+
+float ticketPrice = 10.00F;
+
+Console.Write("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age <= 12 || age >=65)
+    Console.WriteLine($"You are {age} years old and the price is GHS7.00");
+
+else Console.WriteLine($"You are {age} years old and the price is GHS{ticketPrice}.");
